45ea23c5f9e95482e8a0eb6aff4fbef02ae97b65
Alex Salazar <alexwsalazar@gmail.com>
2022-04-13 11:59:09 -0500

    added loging page

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
