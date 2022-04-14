247cce6005e6e8ad04a844ff09500d6a3566f572
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-14 12:15:30 -0500

    commiting before merge.

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+//Here I am going to do the fetch requests from the API
+
+document.querySelector("#upload").addEventListener("click", )
+
+async function fetchPhoto (){
+    console.log("testing")
+}
