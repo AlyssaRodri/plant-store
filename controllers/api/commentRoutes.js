4098dc4bfff506f415c352041be614d2bd78b7f8
Jonathan's Mac Mini <jonathansmacmini@Jonathanss-Mini.attlocal.net>
2022-04-14 19:51:08 -0500

    Alex and I got the Comment Routes, Comment Modules, and Seed Data written.

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newComment = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: "No comment found with this id!" });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
