b7423200eadb8af01ebd6e26b776039a08be6b82
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-15 10:56:41 -0500

    New upload widget added.

--- public/js/imgurAPI.js
+++ public/js/imgurAPI.js
@@ -1,19 +1,10 @@
 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer 5eeae49394cd929e299785c8805bd168fc675280");
 
-var formdata = new FormData();
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: formdata,
-  redirect: 'follow'
-};
-
-fetch("https://www.api.imgur.com/3/upload", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-document.querySelector("upload")
+// create cloudinary account
+// utilize the cloudinary widget--allows for image and video uploads
+// the upload function that cloudinary provides is promise based
+//we upload stuff and it returns a successful insert of the image to their database
+// this data includes the url for where to find it and look at it aka a source
+// we then take that url and store it in OUR database
+// images saved tabled
+// user id, image_src => comes from the cloudinary widget (response.info), title, etc.
