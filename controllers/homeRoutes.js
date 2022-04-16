8fc229de700020e96c03b035ffbcc8e580fb95b8
Alex Salazar <alexwsalazar@gmail.com>
2022-04-16 13:54:49 -0500

    added event listener for comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,6 +51,28 @@ router.get('/blog/:id', async (req, res) => {
   }
 });
 
+router.get('/comment/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const comment = commentData.get({ plain: true });
+
+    res.render('blog', {
+      ...comment,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
@@ -79,6 +101,31 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const commentData = await Comment.findAll({
+       include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('blog', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -37,6 +37,27 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+
+  const text = document.querySelector('#comment-desc').value.trim();
+
+  if (text) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ text }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/blog');
+    } else {
+      alert('Failed to create comment');
+    }
+  }
+};
 
 document
   .querySelector('.new-blog-form')
@@ -45,6 +66,10 @@ document
 document
   .querySelector('.blog-list')
   .addEventListener('click', delButtonHandler);
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newFormHandler);
 
 
 
