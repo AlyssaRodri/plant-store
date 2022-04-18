190d509c27aa8ced81c14a74d9055674cac358aa
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-18 17:19:57 -0500

    Media Cloud Library working!

---
+++ public/javascript/media-cloud.js
@@ -0,0 +1,19 @@
+const mloptions = {
+    cloud_name: 'plantblog',
+    api_key: '278972643829527',
+    username: 'alyssa.taylor.rodriguez@gmail.com',
+    button_class: 'myBtn',
+    button_caption: 'View Our Image Library!',
+    default_transformations: [{quality: "auto"},{format: "auto"}],    
+    insert_transformation: true,
+    }
+
+function insertHandler(data){
+console.log(data);
+}
+
+window.ml = cloudinary.createMediaLibrary(mloptions, {insertHandler: insertHandler}, "#my_btn")
+
+
+
+
